a6779cfb2494ea493db6eab9bb7e1c28fe32c3b1
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2019-02-21 21:58:57 +0300

    base config webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+class App extends React.Component {
+  render() {
+    return <div className="app">hello</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack/webpack.config.main.js
@@ -0,0 +1,35 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new webpack.ProvidePlugin({
+      React: "react",
+      ReactDOM: "react-dom"
+    }),
+    new CleanWebpackPlugin(path.resolve("dist"), { root: path.resolve("../") })
+  ]
+};
