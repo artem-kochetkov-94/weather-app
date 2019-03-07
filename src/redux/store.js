5aae2ac04da9def9f1968220f1dec4823a3dc78d
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2019-03-07 21:21:51 +0300

    redux-devtools-extensions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./index";
 import reduxThunk from "redux-thunk";
 import reduxLogger from "redux-logger";
@@ -7,6 +7,17 @@ const middlewares = [reduxThunk];
 
 if (isDev) middlewares.push(reduxLogger);
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+const store = isDev
+  ? createStore(rootReducer, enhancer)
+  : createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
